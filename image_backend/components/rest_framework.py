e8fb3156907410f7d72eb7a6b3311ffbc9c6ff7c
Zhang <poterasu.zhang@gmail.com>
2022-02-26 02:15:05 +0800

    feat: split settings for each environment

---
+++ image_backend/components/rest_framework.py
@@ -0,0 +1,3 @@
+REST_FRAMEWORK = {
+    'EXCEPTION_HANDLER': 'core.utils.api_service_handler.api_service_handler'
+}
---
+++ image_backend/env_settings/production.py
@@ -0,0 +1,4 @@
+DEBUG = False
+
+# This needs to update with a public IP and/or domain
+ALLOWED_HOSTS = ['*']
