c85f761cf736c13ce951bbe8333447a6fb60817c
Zhang <poterasu.zhang@gmail.com>
2022-02-25 16:21:09 +0800

    feat: fetch images from URL and save to file system

--- api/management/commands/import_csv.py
+++ api/management/commands/import_csv.py
@@ -1,7 +1,10 @@
 import codecs
 import csv
+from tempfile import NamedTemporaryFile
 from urllib import request
+from urllib.request import urlopen
 
+from django.core.files import File
 from django.core.management import BaseCommand
 
 from api.models import Car
@@ -12,17 +15,27 @@ CSV_URL = 'https://docs.google.com/spreadsheets/d/1SYNFV6IGYOme9smQKVBFBPnOQvJa3
 class Command(BaseCommand):
     def handle(self, *args, **options):
         print('Removing the previous car data...')
-        Car.objects.all().delete()
+        for car in Car.objects.all():
+            car.delete()
 
         print('Importing new car data...')
 
         # Fetch CSV file from online URL
-        response = request.urlopen(CSV_URL)
-        csv_rows = list(csv.DictReader(codecs.iterdecode(response, 'utf-8')))
+        try:
+            response = request.urlopen(CSV_URL)
+            csv_rows = list(csv.DictReader(codecs.iterdecode(response, 'utf-8')))
+        except Exception:
+            print('Can not pull the CSV file.')
+            return
+
         print(csv_rows)
 
         for row in csv_rows:
-            car = Car.objects.create(**row)
-            car.save()
+            car = Car.objects.create(title=row.get('title'), description=row.get('description'),
+                                     image_url=row.get('image'))
 
+            print(f'----- Saving: {car} -----')
+
+            car.get_image()
+            car.save()
 
--- api/models.py
+++ api/models.py
@@ -1,10 +1,63 @@
+import os.path
+from tempfile import NamedTemporaryFile
+from urllib.request import urlopen
+
+from django.core.files import File
 from django.db import models
+from imagekit.models import ImageSpecField
+from pilkit.processors import ResizeToFit
 
 
 class Car(models.Model):
-    image = models.CharField(max_length=100)
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=100)
+    image_url = models.URLField(blank=True, null=True)
+    image_file = models.ImageField(upload_to='images', blank=True, default=None)
+    image_web = ImageSpecField(source='image_file',
+                               processors=[ResizeToFit(width=1024)],
+                               format='JPEG',
+                               options={'quality': 88})
+    image_mobile = ImageSpecField(source='image_file',
+                                  processors=[ResizeToFit(width=480)],
+                                  format='JPEG',
+                                  options={'quality': 84})
+
+    def get_image(self):
+        if not self.image_url:
+            print(f'Image URL is empty.')
+            return False
+
+        try:
+            # Request the image URL
+            resp = urlopen(self.image_url)
+
+            # Check the response code
+            if resp.status != 200:
+                print(f'Response is unsuccessful: {self.image_url}')
+                return False
+
+            # Check the content type is image
+            content_type = resp.headers.get_content_type()
+            if not content_type.startswith('image'):
+                print(f'Invalid image: {self.image_url}')
+                return False
+
+            # Save the response to file
+            content = resp.read()
+            img_temp = NamedTemporaryFile(delete=True)
+            img_temp.write(content)
+            img_temp.flush()
+            self.image_file.save(f"image_{self.pk}", File(img_temp))
+            print(f'Successfully saved: {self.image_url}')
+            return True
+        except Exception:
+            print(f'Can not pull the image: {self.image_url}')
+            return False
+
+    def delete(self, *args, **kwargs):
+        if self.image_file and os.path.isfile(self.image_file.path):
+            os.remove(self.image_file.path)
+        super(Car, self).delete(*args, **kwargs)
 
     def __str__(self):
-        return self.title
+        return f'{self.id} - {self.title}'
